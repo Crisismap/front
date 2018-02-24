613886ce1765fe9af3d4d50cd16a4ce70453d6e4
ggolikov <gr.golikov@gmail.com>
2018-02-24 21:54:33 +0300

    Tune clusters settings

--- app/index.js
+++ app/index.js
@@ -2,11 +2,6 @@ import 'leaflet';
 import { MarkerClusterGroup } from 'leaflet.markercluster';
 import * as jsonData from './data/data.geo.json';
 
-var m = document.querySelector('#map');
-m.style.height = document.documentElement.clientHeight + 'px';
-var lp = document.querySelector('.left-panel');
-lp.style.height = document.documentElement.clientHeight + 'px';
-
 let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
@@ -17,13 +12,16 @@ let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
 
 let data = L.geoJson(jsonData, {
         onEachFeature: onEachFeature
-    })//.addTo(lmap);
+    });
 
 function onEachFeature(feature, layer) {
     layer.on('click', (e) => root.innerHTML = e.target.feature.properties.Description);
 }
 
-var markers = L.markerClusterGroup();
+let markers = L.markerClusterGroup({
+    spiderfyOnMaxZoom: true,
+    showCoverageOnHover: false,
+});
 markers.addLayer(data);
 
 lmap.addLayer(markers);
