d04147174e15e4175c210f7ba28ab17877304871
ggolikov <gr.golikov@gmail.com>
2018-02-24 21:15:59 +0300

    Style descriptions

--- app/index.js
+++ app/index.js
@@ -12,18 +12,20 @@ let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }),
     point = L.latLng([55.819723, 37.611661]),
-    lmap = new L.Map('map', {layers: [osm], center: point, zoom: 4, maxZoom: 22}),
-    root = document.getElementById('content');
+    lmap = new L.Map('map', {layers: [osm], center: point, zoom: 2, maxZoom: 22}),
+    root = document.querySelector('.content');
 
 let data = L.geoJson(jsonData, {
         onEachFeature: onEachFeature
     })//.addTo(lmap);
 
 function onEachFeature(feature, layer) {
-    console.log(feature);
+    layer.on('click', (e) => root.innerHTML = e.target.feature.properties.Description);
 }
 
 var markers = L.markerClusterGroup();
 markers.addLayer(data);
 
 lmap.addLayer(markers);
+
+lmap.on('click', () => root.innerHTML = '')
