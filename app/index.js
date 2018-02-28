724abf162f1296bc6d0f4aca6f0b5189e1ad5037
ggolikov <gr.golikov@gmail.com>
2018-02-28 21:26:16 +0300

    Split clusters

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import 'leaflet';
 import { MarkerClusterGroup } from 'leaflet.markercluster';
+import { getRandomColor } from './utils';
 import * as jsonData from './data/data.geo.json';
 
 let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
@@ -8,22 +9,71 @@ let osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
     }),
     point = L.latLng([55.819723, 37.611661]),
     lmap = new L.Map('map', {layers: [osm], center: point, zoom: 2, maxZoom: 22}),
-    root = document.querySelector('.content');
+    root = document.querySelector('.content'),
+    clustersIds = jsonData.features.map(f => f.properties.cluster_id)
+                                   .filter((v, i, a) => a.indexOf(v) === i);
 
-let data = L.geoJson(jsonData, {
-        onEachFeature: onEachFeature
-    });
+    jsonData.features.filter(f => f.properties.cluster_id === 3)
+                     .forEach(f => console.log(f.geometry.coordinates));
 
-function onEachFeature(feature, layer) {
-    layer.on('click', (e) => root.innerHTML = e.target.feature.properties.Description);
+let featureStyle = {
+    radius: 7,
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
 }
 
-let markers = L.markerClusterGroup({
-    spiderfyOnMaxZoom: true,
-    showCoverageOnHover: false,
-});
-markers.addLayer(data);
+let createLayersFromClusters = clusters => {
+    for (let i = 0; i < clusters.length; i++) {
+        featureStyle.fillColor = getRandomColor();
 
-lmap.addLayer(markers);
+        let data = L.geoJson(jsonData, {
+            onEachFeature: (feature, layer) => {
+                layer.on('click', (e) => {
+                    L.DomEvent.stopPropagation(e);
+                    root.innerHTML = e.target.feature.properties.Description;
+                });
+            },
+            pointToLayer: (feature, latlng) => L.circleMarker(latlng, featureStyle),
+            filter: (feature, layer) => feature.properties.cluster_id === clusters[i]
+        });
 
-lmap.on('click', () => root.innerHTML = '')
+        let iconCreateFunction = cluster => {
+		    let childCount = cluster.getChildCount();
+            let c = ' marker-cluster-';
+		        if (childCount < 10) {
+			        c += 'small';
+		        } else if (childCount < 100) {
+			        c += 'medium';
+		        } else {
+			        c += 'large';
+		        }
+
+		    return new L.DivIcon({
+                html: '<div><span>' + childCount + '</span></div>',
+                className: 'marker-cluster' + c,
+                style: {
+                    backgroundColor: featureStyle.fillColor
+                },
+                iconSize: new L.Point(40, 40)
+            });
+        }
+
+        let markers = L.markerClusterGroup({
+            iconCreateFunction: iconCreateFunction,
+            spiderfyOnMaxZoom: true,
+            showCoverageOnHover: false,
+        });
+        markers.addLayer(data);
+
+        lmap.addLayer(markers);
+    }
+}
+
+createLayersFromClusters(clustersIds);
+
+lmap.on('click', () => root.innerHTML = '');
+
+// DEBUG ONLY
+window.lmap = lmap;
